44237357b702e6c5e1c1532734850ed64d390afb
Lena Matoi <51429631+LenaMatoi@users.noreply.github.com>
2024-08-25 21:02:58 +0200

    Cambios en el fichero
    Se han añadido un par de rem para saber donde ahi que tocar.

--- Mantenimiento.bat
+++ Mantenimiento.bat
@@ -1,20 +1,20 @@
-:: .NET Framework 4
-:: Refresco del .NET
+REM .NET Framework 4
+REM Refresco del .NET
 "%windir%\Microsoft.NET\Framework\v4.0.30319\ngen.exe" update /force
 "%windir%\Microsoft.NET\Framework\v4.0.30319\ngentask.exe" /runtimewide
 "%windir%\Microsoft.NET\Framework64\v4.0.30319\ngen.exe" update /force
 "%windir%\Microsoft.NET\Framework64\v4.0.30319\ngentask.exe" /runtimewide
 
-:: Comprobación, limpieza y reparación de archivos del sistema
+REM Comprobación, limpieza y reparación de archivos del sistema
 "%windir%\system32\Dism.exe" /Online /Cleanup-Image /StartComponentCleanup /ResetBase /RestoreHealth
 "%windir%\system32\sfc.exe" /SCANNOW
 
-:: Liberador de espacio en disco clásico
+REM Liberador de espacio en disco clásico
 "%windir%\system32\cleanmgr.exe" /sageset:65535
 "%windir%\system32\cleanmgr.exe" /sagerun:65535
 
-:: Elimina las instantáneas de un volumen
+REM Elimina las instantáneas de un volumen
 vssadmin Delete Shadows /All
 
-:: Defragmentar disco
+REM Defragmentar disco
 "%windir%\system32\dfrgui.exe"
